0792841216f46fe0cd80caf03e10ec8b7bc72c0b
Nicholas <Nicholasshamshum@gmail.com>
2025-01-28 23:41:22 +0200

    Updates made to api
    ## Post new user
    - Post new user and update data.json
    - Also changed data.js to data.json
    ## installed uuid
    - Guid is ramdomly generated when posting a new user
    ## Update data.json when deleting a user
    - Users are removed from data.json

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-import { USER_DATA } from "./data.js";
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
+import user_data from "./data.json" assert { type: "json" };
+import fs from 'fs';
+
 const app = express();
 const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 // Empty array to store data
 let data = [];
@@ -9,7 +15,7 @@ let data = [];
 // Get all users
 app.get("/users", (req, res) => {
   // Copy all user_data to data array
-  data = [...USER_DATA];
+  data = [...user_data];
   res.json(data);
 });
 
@@ -17,7 +23,7 @@ app.get("/users", (req, res) => {
 app.get("/users/:guid", (req, res) => {
   const requested_guid = req.params.guid;
 
-  data = USER_DATA.filter(function (user) {
+  data = user_data.filter((user) => {
     // If user.guid matches requested_guid, return that user's guid
     if (user.guid === requested_guid) {
       return true;
@@ -34,11 +40,12 @@ app.get("/users/:guid", (req, res) => {
   }
 });
 
+// Delete user by guid
 app.delete("/users/:guid", (req, res) => {
   const requested_guid = req.params.guid;
 
   // Find the index of the user
-  const userIndex = USER_DATA.findIndex((user) => {
+  const userIndex = user_data.findIndex((user) => {
     if (user.guid === requested_guid) {
       console.log(`Found user: ${requested_guid}`);
       return true;
@@ -48,11 +55,12 @@ app.delete("/users/:guid", (req, res) => {
 
   // If user is found
   if (userIndex !== -1) {
-    // Store the user data before removing
-    const deletedUser = USER_DATA[userIndex];
+    const deletedUser = user_data[userIndex];
 
-    // Remove the user from the array
-    USER_DATA.splice(userIndex, 1);
+    // This is where we delete the user and then update the data.json file
+    user_data.splice(userIndex, 1);
+    fs.writeFileSync('data.json', JSON.stringify(user_data, null, 2));
+
 
     // Send the deleted user data in response
     res.json(deletedUser);
@@ -61,6 +69,27 @@ app.delete("/users/:guid", (req, res) => {
     // If user is not found
     res.status(404).json({ message: "User not found" });
   }
+});
+
+// Post a new user
+app.post("/users/post", (req, res) => {
+  const { name, phone, email } = req.body;
+
+  // Generate a random GUID
+  const guid = uuidv4();
+
+  // Create a new user object
+  const newUser = {
+    guid,
+    name,
+    phone,
+    email,
+  };
+
+  user_data.push(newUser);
+  res.status(201).json(newUser);
+  fs.writeFileSync('data.json', JSON.stringify(user_data, null, 2));
+  console.log(`User added: ${guid}`);
 });
 
 app.listen(port, () => {
